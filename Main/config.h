610bc02de4bac860e96d7abbc57a21de9c24ad40
miguelromariz <miguelromariz7@gmail.com>
2021-05-28 09:02:42 +0100

    config miguel

--- Main/config.h
+++ Main/config.h
@@ -61,6 +61,32 @@
 
 /**************** MIGUEL ROMARIZ CONFIG ********************/
 
+// LED configuration
+#define LED_1_PIN 14
+#define LED_2_PIN 12
+#define LED_3_PIN 13
+#define LED_4_PIN 15
+
+// Interrupt switch configuration
+#define SWITCH_1_PIN 4
+#define SWITCH_2_PIN 5
+#define SWITCH_3_PIN 2
+#define SWITCH_4_PIN 0
+
+// Network ID's
+#define SWITCH1_ID 9
+#define SWITCH2_ID 1
+#define SWITCH3_ID 11
+#define SWITCH4_ID 12
+#define LED1_ID 9
+#define LED2_ID 10
+#define LED3_ID 11
+#define LED4_ID 12
+
+// Network
+#define SSID "HUAWEI P10";
+#define PASSWORD "2072cac61e06";
+
 
 /**************** PEDRO CONFIG ********************/
 
